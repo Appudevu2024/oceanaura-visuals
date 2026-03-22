f8425d29a9f4d8d26ea1ebea94240aacdffca027
Sandhya Babu <sandhyababu08@gmail.com>
2026-03-22 07:19:50 +0300

    Homepage updated

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
 import Carousel from '../components/Carousel'
-
+import AboutPage from './AboutPage'
+import GalleryPage from './GalleryPage'
+import ServicesPage from './ServicesPage'
+import ContactPage from './ContactPage'
 function HomePage() {
   return (
     <div>
        <Carousel/>
+       <AboutPage/>
+       <GalleryPage/>
+       <ServicesPage/>
+       <ContactPage/>
         
     </div>
   )
